34084cec6147f633a86902bfe104f164ee800db0
Sameer K. Deshpande <sameerd@alum.mit.edu>
2019-03-17 09:14:38 -0400

    Commit before pull

--- scripts/test_km.R
+++ scripts/test_km.R
@@ -10,5 +10,5 @@ load("data/large_example_1.RData")
 
 sourceCpp("src/test_km.cpp")
 
-test <- test_km(ybar_1_large[,1], T = 10, A_block = A_block_large, gamma_init = gamma_1_large, split_frac = 0.5)
+test <- test_km(ybar_1_large[,1], T = 10, A_block = A_block_large, gamma_init = gamma_1_large, reps = 10)
 
